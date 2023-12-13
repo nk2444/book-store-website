f8fcc698a860315cc6f1329e416d18bc077449ae
neelam <nk.neelam096@gmail.com>
2023-12-13 20:14:40 +0530

    initial change

--- shiva/settings.py
+++ shiva/settings.py
@@ -138,12 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# managing media
-MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
-MEDIA_URL= '/media/'
 #added manually
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# managing media
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL= '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
