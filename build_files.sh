7dbbebb6cbd0f56972cb284c9321fccd12d7f156
neelam <nk.neelam096@gmail.com>
2023-12-13 20:07:59 +0530

    initial change

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,9 @@
 echo " BUILD START"
 python 3.12.0 pip install -r requirements.txt
 
+echo " MAKEMIGRATION...."
+python 3.12.0 manage.py makemigrations --noinput --
+python 3.12.0 manage.py migrate--noinput --noinput
 
 python 3.12.0 manage.py collectstatic --noinput --clear
 echo " BUILD END"
