6cdd0cec21c47233da9163c26a206a22156a2b73
neelam <nk.neelam096@gmail.com>
2023-12-13 15:54:36 +0530

    store file

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,2 @@
-echo " BUILD START"
 pip install -r requirements.txt
-
-echo " MAKEMIGRATION...."
-python3.12.0 manage.py makemigrations --noinput --
-python3.12.0 manage.py mmigrate--noinput --noinput
-
-python3.12.0 manage.py collectstatic --noinput --clear
-echo " BUILD END"
+python3.12.0 manage.py collectstatic
